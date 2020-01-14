5fbb0b69b5bd851f8270f967e2e90c13a8005e2e
David Everett <davideverett1989@gmail.com>
2020-01-13 20:32:00 -0600

    Did some more stuff

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 let myButton = document.getElementById("click-me");
 let myDiv = document.getElementById("target");
-let myPrompt = window.prompt("Type Something");
 myButton.addEventListener("click", words);
 
 
 function words() {
-    myDiv.innerHTML = myPrompt;
+    myDiv.innerHTML = `<img src ="https://vignette.wikia.nocookie.net/starwars/images/c/cc/Star-wars-logo-new-tall.jpg/revision/latest?cb=20190313021755" alt="star-wars">`;
 }
