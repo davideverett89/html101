1c8200cba57f3138c4667b8c43268c9a699dcfbf
David Everett <davideverett1989@gmail.com>
2020-01-13 20:21:12 -0600

    Made some changes

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("Whasssssuuuuuppp!?!?!?!");
+let myButton = document.getElementById("click-me");
+let myDiv = document.getElementById("target");
+let myPrompt = window.prompt("Type Something");
+myButton.addEventListener("click", words);
+
+
+function words() {
+    myDiv.innerHTML = myPrompt;
+}
